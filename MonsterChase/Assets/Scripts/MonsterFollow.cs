6ec8a0ba8cdb528b72c56638db70008f6bb5df2b
makazemafuu <102394629+makazemafuu@users.noreply.github.com>
2022-04-04 20:46:48 +0200

    WallCollider
    Manage to make my player and monster + wall hit work ! Now I'll make my whole maze and see if it works with all my walls before making sort of monster trigger where when the player is touched by them, they die and it's game over !

--- MonsterChase/Assets/Scripts/MonsterFollow.cs
+++ MonsterChase/Assets/Scripts/MonsterFollow.cs
@@ -16,4 +16,28 @@ public class MonsterFollow : MonoBehaviour
     {
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Walls")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+        }
+    }
+
 }
